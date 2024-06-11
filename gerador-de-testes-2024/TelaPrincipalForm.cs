ff31cbded65d3a16a91cbc3f744b7da41366daae
Gustavo da Costa Fontana <162226403+gucfontana@users.noreply.github.com>
2024-06-11 17:40:32 -0300

    Add project files.

---
+++ gerador-de-testes-2024/TelaPrincipalForm.cs
@@ -0,0 +1,10 @@
+namespace gerador_de_testes_2024
+{
+    public partial class TelaPrincipalForm : Form
+    {
+        public TelaPrincipalForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
